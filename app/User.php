ea98d8f4ef3f8ef7a86b537ec18355e699edc171
marcelo <lmarceloov@gmail.com>
2018-08-08 22:57:19 -0400

    primer commit

---
+++ app/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+    use SoftDeletes;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $primaryKey = 'usuario_id';
+    protected $fillable = [
+        'nombre', 
+        'cuenta', 
+        'password',
+        'tipo_usuario'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    protected $dates = ['deleted_at'];
+}
